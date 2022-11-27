b1da378f0dd68069e1319dd3ab813c4815f93aa8
Jeff Bowman <jeff@rafflesnow.com>
2022-11-27 13:07:00 -0500

    added the code

---
+++ JeffBotWeb/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+using JeffBot;
+
+namespace JeffBotWeb
+{
+    public class Program
+    {
+        public static List<JeffBot.StreamerSettings> StreamerSettings => new List<StreamerSettings> 
+        {
+            // Add your streamer settings here
+        };
+        
+        public static void Main(string[] args) 
+        {
+            foreach (var streamer in StreamerSettings)
+            {
+                var runMe = new JeffBot.JeffBot(streamer);
+            }
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
