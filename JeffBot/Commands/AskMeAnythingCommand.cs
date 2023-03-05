64cdcbf72f8a4e2f021a260f934d9c6e6e443e93
Jeff Bowman <jeff@rafflesnow.com>
2023-03-05 18:35:48 -0500

    Added ability for AskMeAnything to remember 5 messages back per user

--- JeffBot/Commands/AskMeAnythingCommand.cs
+++ JeffBot/Commands/AskMeAnythingCommand.cs
@@ -14,9 +14,13 @@ namespace JeffBot
 {
     public class AskMeAnythingCommand : BotCommandBase
     {
+        #region UsersContext
+        public Dictionary<string, LimitedQueue<(string prompt, string response)>> UsersContext = new();
+        #endregion
         #region OpenAIAPI
         public OpenAIClient OpenAIClient { get; set; }
         #endregion
+
         #region BotFeature - Override
         public override BotFeatures BotFeature => BotFeatures.AskMeAnything;
         #endregion
@@ -33,21 +37,17 @@ namespace JeffBot
         #region AskAnything
         public async Task AskAnything(ChatMessage chatMessage, string whatToAsk)
         {
-            var chatPrompts = new List<ChatPrompt>
-            {
-                new("system", $"You are {StreamerSettings.StreamerBotName} a bot for the streamer {StreamerSettings.StreamerName} on Twitch. Do NOT exceed 500 characters in any response. {StreamerSettings.AdditionalAIPrompt}."),
-                new("system", $"This message is from the user {chatMessage.DisplayName}."),
-                new("system", $"You will make up an answer, if you don't know the answer."),
-                new("user", $"{whatToAsk}")
-            };
-            var result = await OpenAIClient.ChatEndpoint.GetCompletionAsync(new ChatRequest(chatPrompts, Model.GPT3_5_Turbo, 0.5, maxTokens: 200, presencePenalty: 0.1, frequencyPenalty: 0.1 ));
+            var chatPrompts = GenerateChatPromptsForUser(chatMessage, whatToAsk);
+            var result = await OpenAIClient.ChatEndpoint.GetCompletionAsync(new ChatRequest(chatPrompts, Model.GPT3_5_Turbo, 0.5, maxTokens: 200, presencePenalty: 0.1, frequencyPenalty: 0.1));
+
             Console.WriteLine(result.FirstChoice.Message.Content);
+            UsersContext[chatMessage.Username].LimitedEnqueue((whatToAsk, result.FirstChoice.Message.Content));
             // Twitch messages cannot be longer than 500 characters.. so output multiple messages if the response from the AI is too long
             foreach (Match match in SplitToLines(result.FirstChoice.Message.Content, 500))
             {
                 TwitchChatClient.SendMessage(chatMessage.Channel, $"{match.Value}");
             }
-        } 
+        }
         #endregion
 
         #region ProcessMessage - Override
@@ -56,6 +56,7 @@ namespace JeffBot
             if (IsCommandEnabled)
             {
                 #region Ask Me Anything
+
                 var isMarkWithMessage = Regex.Match(chatMessage.Message.ToLower(), @$"^!{CommandKeyword} .*$");
                 if (isMarkWithMessage.Captures.Count > 0)
                 {
@@ -65,23 +66,50 @@ namespace JeffBot
                         AskAnything(chatMessage, questionOrText.Captures[0].Value.Trim()).Wait();
                     }
                 }
+
                 #endregion
             }
         }
         #endregion
-
         #region Initialize - Override
         public override void Initialize()
         {
-            OpenAIClient = new OpenAIClient(new OpenAIAuthentication("ADD THIS"));
+            OpenAIClient = new OpenAIClient(new OpenAIAuthentication("ADD KEY HERE"));
         }
         #endregion
 
+        #region GenerateChatPromptsForUser
+        private List<ChatPrompt> GenerateChatPromptsForUser(ChatMessage chatMessage, string whatToAsk)
+        {
+            var chatPrompts = new List<ChatPrompt>
+            {
+                new("system", $"You are {StreamerSettings.StreamerBotName} a bot for the streamer {StreamerSettings.StreamerName} on Twitch. Do NOT exceed 500 characters in any response. {StreamerSettings.AdditionalAIPrompt}."),
+                new("system", $"This message is from the user {chatMessage.DisplayName}."),
+                new("system", $"You will make up an answer, if you don't know the answer.")
+            };
+
+            if (UsersContext.ContainsKey(chatMessage.Username))
+            {
+                foreach (var prompt in UsersContext[chatMessage.Username])
+                {
+                    chatPrompts.Add(new ChatPrompt("user", prompt.prompt));
+                    chatPrompts.Add(new ChatPrompt("assistant", prompt.response));
+                }
+            }
+            else
+            {
+                UsersContext[chatMessage.Username] = new LimitedQueue<(string prompt, string response)>(5);
+            }
+
+            chatPrompts.Add(new ChatPrompt("user", $"{whatToAsk}"));
+            return chatPrompts;
+        }
+        #endregion
         #region SplitToLines
-        public MatchCollection SplitToLines(string stringToSplit, int maximumLineLength)
+        private MatchCollection SplitToLines(string stringToSplit, int maximumLineLength)
         {
             return Regex.Matches(stringToSplit, @"(.{1," + maximumLineLength + @"})(?:\s|$)");
-        } 
+        }
         #endregion
     }
 }
