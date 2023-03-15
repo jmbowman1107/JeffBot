9c98d75d279fa3daeb0b026e33ae9c1a004efd90
Jeff Bowman <jeff@rafflesnow.com>
2023-03-15 15:47:17 -0400

    :)

--- JeffBot/AwsUtilities/DynamoDb.cs
+++ JeffBot/AwsUtilities/DynamoDb.cs
@@ -15,23 +15,23 @@ namespace JeffBot.AwsUtilities
 {
     public static class DynamoDb
     {
-#region PopulateOrUpdateStreamerSettings
+        #region PopulateOrUpdateStreamerSettings
         public static async Task PopulateOrUpdateStreamerSettings(StreamerSettings streamerSettings, CancellationToken stoppingToken = default)
         {
-#if DEBUG
+            #if DEBUG
             var chain = new CredentialProfileStoreChain();
             if (!chain.TryGetAWSCredentials("jeff-personal", out var awsCredentials))
             {
                 throw new ArgumentException("No AWS credential profile called 'jeff-personal' was found");
             }
             using var client = new AmazonDynamoDBClient(awsCredentials);
-#else
+            #else
             using var client = new AmazonDynamoDBClient(new AmazonDynamoDBConfig { RegionEndpoint = RegionEndpoint.USEast1 });
-#endif
+            #endif
 
             using var dbContext = new DynamoDBContext(client);
             await dbContext.SaveAsync(streamerSettings, stoppingToken);
         } 
-#endregion
+        #endregion
     }
 }
