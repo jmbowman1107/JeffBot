e2254bf57d30fdfa0a5ef5bf87af653837bc487a
Jeff Bowman <jeff@rafflesnow.com>
2023-04-14 18:14:44 -0400

    Added HttpGet and ToUser command variables

---
+++ JeffBot/Commands/GenericCommand/CommandVariables/HttpGetCommandVariable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using TwitchLib.Client.Models;
+
+namespace JeffBot
+{
+    public class HttpGetCommandVariable : CommandVariableBase
+    {
+        #region Keyword - Override
+        public override string Keyword { get; set; } = "httpget";
+        #endregion
+        #region Description - Override
+        public override string Description { get; set; } = "Use this to send a get request to a URL, will return string (first 100 characters) if string, else will return an error.";
+        #endregion
+        #region UsageExample - Override
+        public override string UsageExample { get; set; } = "{ httpget:https://api.jeffbot.com/samplehttpget }";
+        #endregion
+
+        #region Constructor
+        public HttpGetCommandVariable(BotCommandBase botCommand) : base(botCommand)
+        {
+        }
+        #endregion
+
+        #region ProcessVariable - Override
+        public override async Task<string> ProcessVariable(string command, ChatMessage chatMessage)
+        {
+            if (Uri.IsWellFormedUriString(command, UriKind.Absolute))
+            {
+                var uri = new Uri(command);
+                if (uri.Scheme == Uri.UriSchemeHttps || uri.Scheme == Uri.UriSchemeHttp)
+                {
+                    var httpClient = new HttpClient();
+                    try
+                    {
+                        var result = await httpClient.GetStringAsync(uri);
+                        return result;
+                    }
+                    catch (Exception ex)
+                    {
+                        BotCommand.Logger.LogError($"Error sending GET request to {command}", ex.ToString());
+                        return "Error getting response from server.";
+                    }
+                }
+            }
+            return "Not a valid URL";
+        } 
+        #endregion
+    }
+}
---
+++ JeffBot/Commands/GenericCommand/CommandVariables/ToUserCommandVariable.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using TwitchLib.Client.Models;
+
+namespace JeffBot
+{
+    public class ToUserCommandVariable : CommandVariableBase
+    {
+        #region Keyword - Override
+        public override string Keyword { get; set; } = "touser";
+        #endregion
+        #region Description - Override
+        public override string Description { get; set; } = "Use this variable to show the user who is mentioned in this command, or if there is none, then return the name of the sender.";
+        #endregion
+        #region UsageExample - Override
+        public override string UsageExample { get; set; } = "{ touser }";
+        #endregion
+
+        #region Constructor
+        public UserCommandVariable(BotCommandBase botCommand) : base(botCommand)
+        {
+        }
+        #endregion
+
+        #region ProcessVariable - Override
+        public override async Task<string> ProcessVariable(string command, ChatMessage chatMessage)
+        {
+            var userName = chatMessage.Message.Split(' ').Skip(1).FirstOrDefault();
+            await Task.Run(() => userName ?? chatMessage.DisplayName);
+            return userName ?? chatMessage.DisplayName;
+        } 
+        #endregion
+    }
+}
