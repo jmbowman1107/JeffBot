fe48ab7f6fb52962e07cb788ecb2120a5532f890
Jeff Bowman <jeff@rafflesnow.com>
2023-04-14 17:44:57 -0400

    refactored Worker class to be more readable/maintainable

--- JeffBotWorkerService/Worker.cs
+++ JeffBotWorkerService/Worker.cs
@@ -1,13 +1,9 @@
-#if DEBUG
-using Amazon.Runtime.CredentialManagement;
-#endif
-#if RELEASE
 using Amazon;
-#endif
 using Amazon.DynamoDBv2;
-using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime.CredentialManagement;
 using JeffBot;
 
 namespace JeffBotWorkerService
@@ -18,7 +14,7 @@ namespace JeffBotWorkerService
         private readonly ILoggerFactory _loggerFactory;
 
         #region StreamerSettings
-        public Dictionary<string, (StreamerSettings StreamerSettings, JeffBot.JeffBot JeffBot)> StreamerSettings = new();
+        public Dictionary<string, (StreamerSettings StreamerSettings, JeffBot.JeffBot JeffBot)> StreamerSettings { get; } = new(); 
         #endregion
 
         #region Constructor
@@ -26,11 +22,43 @@ namespace JeffBotWorkerService
         {
             _logger = logger;
             _loggerFactory = loggerFactory;
-        }
+        } 
         #endregion
 
         #region ExecuteAsync - Override
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using var dynamoDbStreamsClient = CreateDynamoDbStreamsClient();
+
+            var globalSettings = await JeffBot.AwsUtilities.DynamoDb.GetGlobalSettings(stoppingToken);
+
+            Singleton<GlobalSettings>.Initialize(globalSettings);
+
+            await LoadStreamerSettings(stoppingToken);
+
+            var shardIterators = new Dictionary<string, string>();
+            var streams = await dynamoDbStreamsClient.ListStreamsAsync(stoppingToken);
+            var stream = streams.Streams.FirstOrDefault(a => a.TableName == "JeffBotStreamerSettings");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (stream == null)
+                {
+                    _logger.LogCritical("Could not find stream for DynamoDB Table 'JeffBotStreamerSettings'");
+                }
+                else
+                {
+                    await LoadDynamoDbStreamShards(shardIterators, dynamoDbStreamsClient, stream, stoppingToken);
+                    await CheckForStreamerUpdatesAndUpdateStreamerSettings(shardIterators, dynamoDbStreamsClient, JeffBot.AwsUtilities.DynamoDb.DbContext, stoppingToken);
+                }
+
+                await Task.Delay(1000, stoppingToken);
+            }
+        } 
+        #endregion
+
+        #region CreateDynamoDbStreamsClient
+        private AmazonDynamoDBStreamsClient CreateDynamoDbStreamsClient()
         {
             #if DEBUG
             var chain = new CredentialProfileStoreChain();
@@ -39,15 +67,17 @@ namespace JeffBotWorkerService
                 _logger.LogCritical("Could not find AWS Credentials");
                 throw new ArgumentException("No AWS credential profile called 'jeff-personal' was found");
             }
-            using var dynamoDbStreamsClient = new AmazonDynamoDBStreamsClient(awsCredentials);
+
+            return new AmazonDynamoDBStreamsClient(awsCredentials);
             #else
-            using var dynamoDbStreamsClient = new AmazonDynamoDBStreamsClient(new AmazonDynamoDBStreamsConfig{ RegionEndpoint = RegionEndpoint.USEast1 });
+            return new AmazonDynamoDBStreamsClient(new AmazonDynamoDBStreamsConfig { RegionEndpoint =
+                RegionEndpoint.USEast1 });
             #endif
-
-            var globalSettings = await JeffBot.AwsUtilities.DynamoDb.GetGlobalSettings(stoppingToken);
-
-            Singleton<GlobalSettings>.Initialize(globalSettings);
-            // TODO: Segment this if we ever need to have a lot of streamers..
+        } 
+        #endregion
+        #region LoadStreamerSettings
+        private async Task LoadStreamerSettings(CancellationToken stoppingToken)
+        {
             var streamerSettings = JeffBot.AwsUtilities.DynamoDb.DbContext.FromScanAsync<StreamerSettings>(new ScanOperationConfig());
             foreach (var streamer in await streamerSettings.GetRemainingAsync(stoppingToken))
             {
@@ -56,33 +86,12 @@ namespace JeffBotWorkerService
                     _logger.LogInformation($"{streamer.StreamerName}'s bot is set not to be active, skipping..");
                     continue;
                 }
+
                 _logger.LogInformation($"Starting bot {streamer.StreamerBotName} for streamer {streamer.StreamerName}");
                 StreamerSettings[streamer.StreamerId] = (streamer, new JeffBot.JeffBot(streamer, _loggerFactory.CreateLogger<JeffBot.JeffBot>()));
             }
-
-            // For watching for changes to bot settings.
-            var shardIterators = new Dictionary<string, string>();
-            var streams = await dynamoDbStreamsClient.ListStreamsAsync(stoppingToken);
-            var stream = streams.Streams.FirstOrDefault(a => a.TableName == "JeffBotStreamerSettings");
-
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                // Watch for settings changes from DynamoDb
-                if (stream == null)
-                {
-                    _logger.LogCritical("Could not find stream for DynamoDB Table 'JeffBotStreamerSettings'");
-                }
-                else
-                {
-                    // TODO: When this scales beyond need of a single container, can we utilize a combination of an update (date time?) and System.Environment.MachineName to determine if a bot should be added?
-                    await LoadDynamoDbStreamShards(shardIterators, dynamoDbStreamsClient, stream, stoppingToken);
-                    await CheckForStreamerUpdatesAndUpdateStreamerSettings(shardIterators, dynamoDbStreamsClient, JeffBot.AwsUtilities.DynamoDb.DbContext, stoppingToken);
-                }
-                await Task.Delay(1000, stoppingToken);
-            }
-        }
+        } 
         #endregion
-
         #region LoadDynamoDbStreamShards
         private static async Task LoadDynamoDbStreamShards(Dictionary<string, string> shardIterators, AmazonDynamoDBStreamsClient dynamoDbStreamsClient, StreamSummary stream, CancellationToken stoppingToken = default)
         {
@@ -100,7 +109,7 @@ namespace JeffBotWorkerService
                     shardIterators[shard.ShardId] = iterator.ShardIterator;
                 }
             }
-        }
+        } 
         #endregion
         #region CheckForStreamerUpdatesAndUpdateStreamerSettings
         private async Task CheckForStreamerUpdatesAndUpdateStreamerSettings(Dictionary<string, string> shardIterators, AmazonDynamoDBStreamsClient dynamoDbStreamsClient, DynamoDBContext dbContext, CancellationToken stoppingToken = default)
@@ -117,47 +126,58 @@ namespace JeffBotWorkerService
                 shardIterators[shardIterator.Key] = updates.NextShardIterator;
                 foreach (var update in updates.Records)
                 {
-                    var streamerThatWasUpdated = update.Dynamodb.Keys["StreamerId"].S;
-                    var newStreamerSettings = await dbContext.LoadAsync<StreamerSettings>(streamerThatWasUpdated, stoppingToken);
-
-                    _logger.LogInformation($"Updated setting for streamer {newStreamerSettings.StreamerName}");
-                    var jeffBotLogger = _loggerFactory.CreateLogger<JeffBot.JeffBot>();
-                    if (StreamerSettings.ContainsKey(streamerThatWasUpdated))
-                    {
-                        if (ShouldRestartBot(StreamerSettings[streamerThatWasUpdated].StreamerSettings, newStreamerSettings))
-                        {
-                            StreamerSettings[streamerThatWasUpdated].JeffBot.ShutdownBotForStreamer();
-                            if (newStreamerSettings.IsActive)
-                            {
-                                StreamerSettings[streamerThatWasUpdated] = (newStreamerSettings, new JeffBot.JeffBot(newStreamerSettings, jeffBotLogger));
-                            }
-                        }
-                        else
-                        {
-                            // TODO: How to only update bot command settings, and not instantiate a new instance of a command? Or maybe just say fuck it?
-                            StreamerSettings[streamerThatWasUpdated] = (newStreamerSettings, StreamerSettings[streamerThatWasUpdated].JeffBot);
-                            StreamerSettings[streamerThatWasUpdated].JeffBot.StreamerSettings = newStreamerSettings;
-                            StreamerSettings[streamerThatWasUpdated].JeffBot.BotCommands = new List<IBotCommand>();
-                            StreamerSettings[streamerThatWasUpdated].JeffBot.InitializeBotCommands();
-                        }
-                    }
-                    else
-                    {
-                        // TODO: When scaling work is done, don't automatically create it, determine if should be doing so.
-                        if (newStreamerSettings.IsActive)
-                        {
-                            StreamerSettings[streamerThatWasUpdated] = (newStreamerSettings, new JeffBot.JeffBot(newStreamerSettings, jeffBotLogger));
-                        }
-                    }
+                    await UpdateStreamerSettings(update, dbContext, stoppingToken);
                 }
             }
-        }
+        } 
+        #endregion
+        #region UpdateStreamerSettings
+        private async Task UpdateStreamerSettings(Record update, DynamoDBContext dbContext, CancellationToken stoppingToken)
+        {
+            var streamerThatWasUpdated = update.Dynamodb.Keys["StreamerId"].S;
+            var newStreamerSettings = await dbContext.LoadAsync<StreamerSettings>(streamerThatWasUpdated, stoppingToken);
+
+            _logger.LogInformation($"Updated setting for streamer {newStreamerSettings.StreamerName}");
+            var jeffBotLogger = _loggerFactory.CreateLogger<JeffBot.JeffBot>();
+
+            if (StreamerSettings.ContainsKey(streamerThatWasUpdated))
+            {
+                if (ShouldRestartBot(StreamerSettings[streamerThatWasUpdated].StreamerSettings, newStreamerSettings))
+                {
+                    StreamerSettings[streamerThatWasUpdated].JeffBot.ShutdownBotForStreamer();
+                    if (newStreamerSettings.IsActive)
+                    {
+                        StreamerSettings[streamerThatWasUpdated] = (newStreamerSettings, new JeffBot.JeffBot(newStreamerSettings, jeffBotLogger));
+                    }
+                }
+                else
+                {
+                    UpdateBotCommandsOnly(streamerThatWasUpdated, newStreamerSettings);
+                }
+            }
+            else
+            {
+                if (newStreamerSettings.IsActive)
+                {
+                    StreamerSettings[streamerThatWasUpdated] = (newStreamerSettings, new JeffBot.JeffBot(newStreamerSettings, jeffBotLogger));
+                }
+            }
+        } 
+        #endregion
+        #region UpdateBotCommandsOnly
+        private void UpdateBotCommandsOnly(string streamerThatWasUpdated, StreamerSettings newStreamerSettings)
+        {
+            StreamerSettings[streamerThatWasUpdated] = (newStreamerSettings, StreamerSettings[streamerThatWasUpdated].JeffBot);
+            StreamerSettings[streamerThatWasUpdated].JeffBot.StreamerSettings = newStreamerSettings;
+            StreamerSettings[streamerThatWasUpdated].JeffBot.BotCommands = new List<IBotCommand>();
+            StreamerSettings[streamerThatWasUpdated].JeffBot.InitializeBotCommands();
+        } 
         #endregion
         #region ShouldRestartBot
         private bool ShouldRestartBot(StreamerSettings oldSettings, StreamerSettings newSettings)
         {
             return oldSettings.HaveRebootRequiredPropertiesChanged(newSettings) || HaveNewBotFeatures(oldSettings.BotFeatures, newSettings.BotFeatures);
-        }
+        } 
         #endregion
         #region HaveNewBotFeatures
         private bool HaveNewBotFeatures(List<BotCommandSettings> oldFeatures, List<BotCommandSettings> newFeatures)
@@ -180,6 +200,7 @@ namespace JeffBotWorkerService
                     return true;
                 }
             }
+
             return false;
         } 
         #endregion
