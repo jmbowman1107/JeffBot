0818076efcb395b001ce29d2051164113346c87b
Jeff Bowman <jeff@rafflesnow.com>
2023-04-15 17:06:02 -0400

    Can now parse JSON given a token path when doing a HTTP GET

--- JeffBot/Commands/GenericCommand/CommandVariables/HttpGetCommandVariable.cs
+++ JeffBot/Commands/GenericCommand/CommandVariables/HttpGetCommandVariable.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using TwitchLib.Client.Models;
 
 namespace JeffBot
 {
     public class HttpGetCommandVariable : CommandVariableBase
     {
+        private static readonly HttpClient httpClient = new HttpClient();
+
         #region Keyword - Override
         public override string Keyword { get; set; } = "httpget";
         #endregion
@@ -27,25 +32,63 @@ namespace JeffBot
         #region ProcessVariable - Override
         public override async Task<string> ProcessVariable(string command, ChatMessage chatMessage)
         {
-            if (Uri.IsWellFormedUriString(command, UriKind.Absolute))
+            (string jsonTokenPath, string url) = SplitCommand(command);
+
+            if (!IsWellFormedUrl(url)) return "Not a valid URL";
+
+            try
             {
-                var uri = new Uri(command);
-                if (uri.Scheme == Uri.UriSchemeHttps || uri.Scheme == Uri.UriSchemeHttp)
-                {
-                    var httpClient = new HttpClient();
-                    try
-                    {
-                        var result = await httpClient.GetStringAsync(uri);
-                        return result;
-                    }
-                    catch (Exception ex)
-                    {
-                        BotCommand.Logger.LogError($"Error sending GET request to {command}", ex.ToString());
-                        return "Error getting response from server.";
-                    }
-                }
+                string result = await FetchData(url);
+                return jsonTokenPath == null ? result : ExtractJsonTokenValue(result, jsonTokenPath);
             }
-            return "Not a valid URL";
+            catch (Exception ex)
+            {
+                BotCommand.Logger.LogError($"Error processing command: {command}", ex);
+                return "Error getting response from server.";
+            }
+        } 
+        #endregion
+
+        #region SplitCommand
+        private (string, string) SplitCommand(string command)
+        {
+            var nonUrlColonPattern = @"(?<!https?):|(?<=\s):|(^:)";
+            var parts = Regex.Split(command, nonUrlColonPattern);
+
+            return parts.Length > 1 ? (parts[0].Trim(), parts[1].Trim()) : (null, command);
+        }
+        #endregion
+        #region IsWellFormedUrl
+        private bool IsWellFormedUrl(string url)
+        {
+            return Uri.IsWellFormedUriString(url, UriKind.Absolute)
+                   && (new Uri(url).Scheme == Uri.UriSchemeHttps || new Uri(url).Scheme == Uri.UriSchemeHttp);
+        }
+        #endregion
+        #region FetchData
+        private async Task<string> FetchData(string url)
+        {
+            return await httpClient.GetStringAsync(new Uri(url));
+        }
+        #endregion
+        #region ExtractJsonTokenValue
+        private string ExtractJsonTokenValue(string json, string tokenPath)
+        {
+            try
+            {
+                var jObject = JObject.Parse(json);
+                return (string)jObject.SelectToken(tokenPath, true);
+            }
+            catch (JsonException ex)
+            {
+                BotCommand.Logger.LogError($"Error parsing JSON output: {json}", ex);
+                return $"Error parsing JSON output. - {ex.Message}";
+            }
+            catch (Exception ex)
+            {
+                BotCommand.Logger.LogError($"Error parsing JSON output: {json}", ex);
+                return $"Error parsing JSON output.";
+            }
         } 
         #endregion
     }
