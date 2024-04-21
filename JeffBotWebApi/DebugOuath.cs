1fda8a6ae96dfb74237460e637b6cab12a3c50e0
Jeff Bowman <jeff@rafflesnow.com>
2024-04-21 12:02:34 -0400

    updated nugets

--- JeffBotWebApi/DebugOuath.cs
+++ JeffBotWebApi/DebugOuath.cs
@@ -1,7 +1,6 @@
 ﻿using System.Text.Encodings.Web;
 using AspNet.Security.OAuth.Spotify;
 using AspNet.Security.OAuth.Twitch;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.Extensions.Options;
 
@@ -9,7 +8,7 @@ namespace JeffBotWebApi
 {
     public class DebugSpotifyOauthHandler : SpotifyAuthenticationHandler
     {
-        public DebugSpotifyOauthHandler(IOptionsMonitor<SpotifyAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        public DebugSpotifyOauthHandler(IOptionsMonitor<SpotifyAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder) : base(options, logger, encoder)
         {
         }
 
@@ -22,7 +21,7 @@ namespace JeffBotWebApi
 
     public class DebugTwitchOauthHandler : TwitchAuthenticationHandler
     {
-        public DebugTwitchOauthHandler(IOptionsMonitor<TwitchAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        public DebugTwitchOauthHandler(IOptionsMonitor<TwitchAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder) : base(options, logger, encoder)
         {
         }
 
